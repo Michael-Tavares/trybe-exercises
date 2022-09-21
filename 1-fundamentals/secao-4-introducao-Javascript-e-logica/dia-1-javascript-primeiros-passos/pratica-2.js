c0fa98d666676ae15ff9b1a65859d079018855e0
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-21 15:43:41 -0300

    Adicionado pratica-2.js

---
+++ 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-1-javascript-primeiros-passos/pratica-2.js
@@ -0,0 +1,22 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof(patientId));
+console.log(typeof(isEnrolled));
+console.log(typeof(patientEmail));
+console.log(typeof(patientInfo));
+
+const base = 5;
+const height = 8;
+const area = base * height;
+
+console.log(area);
+
+const perimeter = (2 * base) + (2 * height);
+
+console.log(perimeter);
