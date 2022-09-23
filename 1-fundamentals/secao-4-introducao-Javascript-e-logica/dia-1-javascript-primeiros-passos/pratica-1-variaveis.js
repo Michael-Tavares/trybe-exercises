4f6ee7be9f3c5da5a65658d071a0c4d59c066f05
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-23 13:38:50 -0300

    Adicionado arquivos

---
+++ 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-1-javascript-primeiros-passos/pratica-1-variaveis.js
@@ -0,0 +1,16 @@
+const myName = 'Michael Douglas';
+const birthCity = 'Brasília';
+let birthYear = 2000;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+/*
+birthCity = 'Cascavel';
+console.log(birthCity);
+
+Este código acima não funciona por que atribuí à uma variável constante,
+sendo que as variáveis constantes não são modificáveis.
+*/
