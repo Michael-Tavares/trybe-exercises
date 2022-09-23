843c2c3ca39961dd4642c8c3f51c293ef6674f95
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-23 18:24:30 -0300

    Exercícios parte 1

--- 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-1-javascript-primeiros-passos/exercicio-1-criar-5-minis-programas.js
+++ 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-1-javascript-primeiros-passos/exercicio-1-criar-5-minis-programas.js
@@ -119,3 +119,39 @@ switch(peca.toLowerCase(String)){
     movimento = 'peça invalida';
 }
 console.log(movimento);
+
+/*
+Utilize if/else para fazer um programa que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+
+Porcentagem >= 80 -> B
+
+Porcentagem >= 70 -> C
+
+Porcentagem >= 60 -> D
+
+Porcentagem >= 50 -> E
+
+Porcentagem < 50 -> F
+
+O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+
+*/
+
+let porcentagem = 40;
+
+if(porcentagem >= 90 && porcentagem < 100){
+  console.log('A');
+}else if(porcentagem >= 80 && porcentagem < 90){
+  console.log('B');
+}else if(porcentagem >= 70 && porcentagem < 80){
+  console.log('C');
+}else if(porcentagem >= 60 && porcentagem < 70){
+  console.log('D');
+}else if(porcentagem >= 50 && porcentagem < 60){
+  console.log('E');
+}else if(porcentagem < 50 && porcentagem > 0){
+  console.log('F');
+}else{
+  console.log('Numero inválido')
+}
