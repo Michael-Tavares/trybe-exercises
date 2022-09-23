7747326f9cc4f39a827afde46601a501810bfca9
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-23 18:17:48 -0300

    Adicionado exercicio xadrez

--- 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-1-javascript-primeiros-passos/exercicio-1-criar-5-minis-programas.js
+++ 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-1-javascript-primeiros-passos/exercicio-1-criar-5-minis-programas.js
@@ -72,10 +72,50 @@ if(valor == positivo){
 Utilize if/else para fazer um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
 */
 
-const primeiro = 60;
+const primeiro = 50;
 const segundo = 60;
 const terceiro = 60;
 
 if(primeiro == 60 && segundo == 60 && terceiro == 60){
   console.log('o triangulo é verdadeiro');
+}else{
+  console.log('Triangulo falso');
 }
+
+
+/*Utilize switch/case para fazer um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+
+Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case).
+
+Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+
+Exemplo: bishop (bispo) -> diagonals (diagonais)
+*/
+
+let peca = 'RAINHA';
+let movimento;
+
+switch(peca.toLowerCase(String)){
+  case 'pẽao':
+    movimento = 'O pẽao pula uma casa para frente e come em diagonal';
+    break;
+  case 'bispo':
+    movimento = 'O bispo anda apenas em diagonal';
+    break;
+  case 'cavalo':
+    movimento = 'O cavalo anda em L';
+    break;
+  case 'torre':
+    movimento = 'A torre anda apenas horizontal';
+    break;
+  case 'rainha':
+    movimento = 'A rainha anda em todas as direções possíveis sem limite';
+    break;
+  case 'rei':
+    movimento = 'o rei anda em todas as direões próximas sem que fique em xeque';
+    break;
+  default:
+    movimento = 'peça invalida';
+}
+console.log(movimento);
