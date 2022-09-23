cf07877c3aae5f90d1a1a4c2c53f08aa566d93b2
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-23 13:44:47 -0300

    Alteração arquivos

--- 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-1-javascript-primeiros-passos/pratica3.js
+++
@@ -1,9 +0,0 @@
-const pessoaCandidata = 60;
-
-if(pessoaCandidata >= 80){
-  console.log('Parabéns, você foi aprovado(a)!');
-}else if(pessoaCandidata < 80 && pessoaCandidata >= 60){
-  console.log('Você está na nossa lista de espera');
-}else{
-  console.log('Você foi reprovado(a)');
-}
