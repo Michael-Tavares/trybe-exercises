cafe251792d8aa786ab72ebbb4340df0975e3c77
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-25 18:49:40 -0300

    Calcula a media

--- 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-2-javascript-array-e-loop-for/exercicio-1-lidando-com-arrays.js
+++ 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-2-javascript-array-e-loop-for/exercicio-1-lidando-com-arrays.js
@@ -1,12 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
+/*Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
 
-//for(let index = 0; index < numbers.length; index += 1){
-//console.log(numbers[index]);
-//}
+for(let index = 0; index < numbers.length; index += 1){
+  console.log(numbers[index]);
+}
+*/
 
-//Some todos os valores contidos no array e imprima o resultado;
+
+
+
+/*Some todos os valores contidos no array e imprima o resultado;
 soma = 0;
 
 for(let index = 0; index < numbers.length; index += 1){
@@ -14,3 +18,41 @@ for(let index = 0; index < numbers.length; index += 1){
 }
 
 console.log(soma);
+*/
+
+
+
+
+/*
+Calcule e imprima a média aritmética dos valores contidos no array;
+  A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
+
+soma = 0;
+media = 0;
+
+for (let index = 0; index < numbers.length; index += 1){
+  soma += numbers[index];
+  media = soma / index;
+}
+
+console.log(media);
+*/
+
+
+
+
+
+/*
+Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+*/
+
+soma = 0;
+media = 0;
+
+for (let index = 0; index < numbers.length; index += 1){
+  soma += numbers[index];
+  media = soma / index;
+}
+
+console.log(media);
