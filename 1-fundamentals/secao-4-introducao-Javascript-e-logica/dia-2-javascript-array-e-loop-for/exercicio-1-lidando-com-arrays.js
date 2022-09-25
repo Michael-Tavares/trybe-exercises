1365af2bd79a955f592384880296a7097c3728d1
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-25 18:44:00 -0300

    Adicionado soma

---
+++ 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-2-javascript-array-e-loop-for/exercicio-1-lidando-com-arrays.js
@@ -0,0 +1,16 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
+
+//for(let index = 0; index < numbers.length; index += 1){
+//console.log(numbers[index]);
+//}
+
+//Some todos os valores contidos no array e imprima o resultado;
+soma = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+  soma += numbers[index];
+}
+
+console.log(soma);
