2b056db83ee89c1a20c34960cbc5e45f107b6ef7
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-26 19:17:28 -0300

    Adicionado função para descobrir indice com menor valor

--- 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-4-javascript-objetos-e-funcoes/exercicio-3-funcoes.js
+++ 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-4-javascript-objetos-e-funcoes/exercicio-3-funcoes.js
@@ -38,3 +38,23 @@ function maiorValor(numeros){
 }
 
 console.log(maiorValor([2, 3, 6, 7, 10, 1]));
+
+/*
+Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+Array de teste: [2, 4, 6, 7, 10, 0, -3];.
+
+Valor esperado no retorno da função: 6.
+*/
+
+function menorValor(numeros){
+  let menor = 0;
+  for(let index in numeros){
+    if(numeros[menor] > numeros [index]){
+      menor = index;
+    }
+  }
+  return menor;
+}
+
+console.log(menorValor([2, 4, 6, 7, 10, 0, -3]));
