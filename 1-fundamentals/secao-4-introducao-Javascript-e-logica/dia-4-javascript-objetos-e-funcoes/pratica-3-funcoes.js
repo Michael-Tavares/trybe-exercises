7a8f34b09d8f3cfc8f0a0159f967eab36b4e3bf3
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-26 15:51:17 -0300

    funções para operação aritmética

---
+++ 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-4-javascript-objetos-e-funcoes/pratica-3-funcoes.js
@@ -0,0 +1,37 @@
+/*Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)
+*/
+
+function somar(a, b){
+  return console.log(a + b);
+}
+somar(30, 40);
+
+
+function subtrair(a, b){
+  return console.log(a - b);
+}
+subtrair(100, 24);
+
+
+function multiplicar(a, b){
+  return console.log(a * b);
+}
+multiplicar(40, 8);
+
+
+function dividir(a, b){
+  return console.log(a / b);
+}
+dividir(700, 5);
+
+
+function resto(a, b){
+  return console.log(a % b);
+}
+resto(100, 10);
