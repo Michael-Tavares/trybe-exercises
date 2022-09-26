312447bc632c725e369c3f37150ce8849c3ea614
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-26 17:04:46 -0300

    Adicionado 2 etapas do exercício

---
+++ 1-fundamentals/secao-4-introducao-Javascript-e-logica/dia-4-javascript-objetos-e-funcoes/exercicio-1-objetos-e-for-in.js
@@ -0,0 +1,14 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+//Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave.
+
+console.log('Bem-vinda, ' + info.personagem);
+
+//Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console.
+
+info['recorrente'] = 'Sim';
+console.log(info);
