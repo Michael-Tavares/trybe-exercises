c9791acef3f9c7d51fe8a9d789b93c26441aa75b
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-28 15:23:15 -0300

    Pratica 1 de DOM

---
+++ 1-fundamentals/secao-5-Javascript-DOM-eventos-webstorage/dia-1-javascript-DOM-e-seletores/pratica-1/js/script.js
@@ -0,0 +1,11 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText = 'Os bons companheiros';
+
+let secondParagraph = document.getElementById('second-paragraph');
+secondParagraph.style.fontSize = '40px';
+secondParagraph.innerText = 'Uma boa síntese';
+
+let subtitle = document.getElementById('subtitle');
+subtitle.style.backgroundColor = 'blue';
