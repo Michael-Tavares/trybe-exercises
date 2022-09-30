85734090aa96d1a37f359ae776aad15f20faac5a
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-30 18:42:17 -0300

    Adicionado lista de dias e classe day

--- 1-fundamentals/secao-5-Javascript-DOM-eventos-webstorage/dia-3-javascript-eventos/exercicio-1/script.js
+++ 1-fundamentals/secao-5-Javascript-DOM-eventos-webstorage/dia-3-javascript-eventos/exercicio-1/script.js
@@ -13,4 +13,16 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+const daysWeek = document.getElementById('days');
+
+for (index = 0; index < decemberDaysList.length; index += 1) {
+  let dias = decemberDaysList[index];
+  let listaDias = document.createElement('li');
+  
+  listaDias.innerHTML = dias;
+  listaDias.className = 'day';
+
+  daysWeek.appendChild(listaDias);
+}
