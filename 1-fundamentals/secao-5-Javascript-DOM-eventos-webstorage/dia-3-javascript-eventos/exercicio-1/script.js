416b1d924b2471e05d403f6b1148db0b92710259
Michael Douglas <michael.douglas1015@gmail.com>
2022-09-30 18:52:40 -0300

    Adicionado função, holiday e friday

--- 1-fundamentals/secao-5-Javascript-DOM-eventos-webstorage/dia-3-javascript-eventos/exercicio-1/script.js
+++ 1-fundamentals/secao-5-Javascript-DOM-eventos-webstorage/dia-3-javascript-eventos/exercicio-1/script.js
@@ -13,16 +13,28 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+function createDays() {
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-const daysWeek = document.getElementById('days');
+  const daysWeek = document.getElementById('days');
 
-for (index = 0; index < decemberDaysList.length; index += 1) {
-  let dias = decemberDaysList[index];
-  let listaDias = document.createElement('li');
-  
-  listaDias.innerHTML = dias;
-  listaDias.className = 'day';
+  for (index = 0; index < decemberDaysList.length; index += 1) {
+    let dias = decemberDaysList[index];
+    let listaDias = document.createElement('li');
+    
+    listaDias.innerHTML = dias;
+    listaDias.className = 'day' + decemberDaysList[index];
 
-  daysWeek.appendChild(listaDias);
+    if (listaDias.className === 'day24' || listaDias.className === 'day25' || listaDias.className === 'day31') {
+      listaDias.classList.add('holiday');
+    }
+
+    if (listaDias.className === 'day4' || listaDias.className === 'day11' || listaDias.className === 'day18' || listaDias.className === 'day25 holiday') {
+      listaDias.classList.add('friday');
+    }
+
+    daysWeek.appendChild(listaDias);
+  }
 }
+
+createDays();
